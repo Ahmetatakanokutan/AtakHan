a9180bde553b663b50cdb66787c1892eb45067d2
ahmetatakanokutan <aatakants611@gmail.com>
2024-06-01 06:09:10 +0300

    web sitesi guncellendi ve yeni hali ayarlandi

--- AtakHanArduinoKodlari/alici/alici.ino
+++ AtakHanArduinoKodlari/alici/alici.ino
@@ -1,4 +1,4 @@
-#include<Servo.h>
+#include <Servo.h>
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
@@ -11,17 +11,11 @@ Servo SolKanat;
 Servo YavasDonus;
 Servo Kalkis;
 char str[32];
-int SagKanatDeger = 90;
-int SolKanatDeger = 90;
-int YavasDonusDeger = 90;
-int KalkisDeger = 90;
-unsigned long YavYonEtkZam = 0;
-unsigned long YatayEtkZam = 0;
-unsigned long DikeyEtkZam = 0;
-long ZamanKont;
+unsigned long lastReceivedTime = 0; // Son veri alım zamanı
+unsigned long connectionTimeout = 3000; // Bağlantı kesilme süresi (3 saniye)
+bool connectionLost = false;
 
 void setup() {
-
   Serial.begin(19200);
   Brushless.attach(8);
   Kalkis.attach(7);
@@ -29,7 +23,7 @@ void setup() {
   SagKanat.attach(5);
   YavasDonus.attach(4);
   radio.begin();
-  radio.setDataRate( RF24_250KBPS );
+  radio.setDataRate(RF24_250KBPS);
   radio.setPALevel(RF24_PA_MIN);
   radio.openReadingPipe(1, address);
   radio.startListening();
@@ -37,86 +31,75 @@ void setup() {
 }
 
 void loop() {
-  
-   if (radio.available()) {
-    radio.read( &str, sizeof(str) );
+  if (radio.available()) {
+    radio.read(&str, sizeof(str));
     Serial.print(str);
 
+    // Son veri alım zamanını güncelle
+    lastReceivedTime = millis();
+    connectionLost = false;
+
     // Parçalanmış veriyi tutacak değişkenler
-    char command[10];
-    char valueStr[10];
+    int throttle = 0;
+    int rudder = 0;
+    int elevator = 0;
+    int aileronRight = 0;
+    int aileronLeft = 0;
 
-    // Komutun başında "motor:" var mı kontrol et
-    if (strncmp(str, "motor:", 6) == 0) {
-      // Gelen değeri al
-      char* valuePtr = str + 6; // "motor:" kısmını atla
-      int value = atoi(valuePtr);
-      
-      // Gelen değeri kontrol et
-      if (value >= 0 && value <= 100) {
-        // Değeri uygun aralığa map et ve servo pozisyonunu ayarla
-        int mappedValue = map(value, 0, 100, 1100, 1280);
-        Brushless.write(mappedValue);
-      }
+    // Gelen veriyi ayrıştır
+    sscanf(str, "%d/%d/%d/%d/%d", &throttle, &rudder, &elevator, &aileronRight, &aileronLeft);
+
+    // Throttle kontrolü
+    if (throttle >= 0 && throttle <= 100) {
+      int mappedThrottle = map(throttle, 0, 100, 0, 180);
+      Brushless.write(mappedThrottle);
     }
 
+    // Rudder kontrolü
+    if (rudder >= -50 && rudder <= 50) {
+      int mappedRudder = map(rudder, -50, 50, 40, 140);
+      YavasDonus.write(mappedRudder);
+    }
+
+    // Elevator kontrolü
+    if (elevator >= -50 && elevator <= 50) {
+      int mappedElevator = map(elevator, -50, 50, 40, 140);
+      Kalkis.write(mappedElevator);
+    }
+
+    // Aileron Right kontrolü
+    if (aileronRight >= -50 && aileronRight <= 50) {
+      int mappedAileronRight = map(aileronRight, -50, 50, 40, 140);
+      SagKanat.write(mappedAileronRight);
+    }
+
+    // Aileron Left kontrolü
+    if (aileronLeft >= -50 && aileronLeft <= 50) {
+      int mappedAileronLeft = map(aileronLeft, -50, 50, 40, 140);
+      SolKanat.write(mappedAileronLeft);
+    }
+  } else { // Eğer bağlantı kesilirse
+    unsigned long currentMillis = millis();
+
+    // Bağlantı kesilme süresi kontrolü
+    if (currentMillis - lastReceivedTime >= connectionTimeout) {
+      if (!connectionLost) {
+        connectionLost = true;
+        YavasDonus.write(110); // YavasDonus servosunu 110 değerine ayarla
+        delay(2000); // 2 saniye bekle
+        YavasDonus.write(90); // YavasDonus servosunu 90 değerine geri getir
+      }
+    } else if (currentMillis - lastReceivedTime >= 500) { // Kısa süreli bağlantı kesilmesi
+      YavasDonus.write(90);
+      SagKanat.write(90);
+      SolKanat.write(90);
+      Kalkis.write(90);
+    }
     
-      if (!strcmp(str, "asagi")) {
-        if(KalkisDeger > 40){
-
-        KalkisDeger -= 5;
-        Kalkis.write(KalkisDeger);
-        }
-        DikeyEtkZam = millis();
-      }
-      
-    else if(!strcmp(str, "yukari")) {
-      if(KalkisDeger <= 140){
-        KalkisDeger += 5;
-        Kalkis.write(KalkisDeger);
-      }
-      DikeyEtkZam = millis();
-    }
-
-
-    if (!strcmp(str, "sag")) {
-      if (SagKanatDeger > 40 && SolKanatDeger > 40) {
-        SagKanatDeger -= 5;
-        SolKanatDeger -= 5;
-        SagKanat.write(SagKanatDeger);
-        SolKanat.write(SolKanatDeger);
-      }
-      YatayEtkZam = millis();
-    }
-    else if (!strcmp(str, "sol")) {
-      if (SolKanatDeger < 140 && SagKanatDeger < 140) {
-        SolKanatDeger += 5;
-        SagKanatDeger += 5;
-        SagKanat.write(SagKanatDeger);
-        SolKanat.write(SolKanatDeger);
-      }
-      YatayEtkZam = millis();
-    }
-    if(!strcmp(str, "yavasol")){
-      if(YavasDonusDeger <140){
-        YavasDonusDeger += 5;
-        YavasDonus.write(YavasDonusDeger);
-      }
-      YavYonEtkZam = millis();
-    }
-    else if(!strcmp(str, "yavasag")){
-      if(YavasDonusDeger > 40){
-        YavasDonusDeger -= 5;
-        YavasDonus.write(YavasDonusDeger);
-      }
-      YavYonEtkZam = millis();
-    }
-  }
-  else { // Eğer bağlantı kesilirse
     // Yeniden bağlantı kurma işlemleri
     if (!radio.isChipConnected()) {
       radio.begin();
-      radio.setDataRate( RF24_250KBPS );
+      radio.setDataRate(RF24_250KBPS);
       radio.setPALevel(RF24_PA_MIN);
       radio.openReadingPipe(1, address);
       radio.startListening();
@@ -124,28 +107,29 @@ void loop() {
     }
   }
 
+  // ZamanKont işlemleri
+  static unsigned long YavYonEtkZam = 0;
+  static unsigned long YatayEtkZam = 0;
+  static unsigned long DikeyEtkZam = 0;
+  long ZamanKont;
 
   ZamanKont = (YavYonEtkZam + 100) - millis();
-  if(ZamanKont < 0){
-    YavasDonusDeger = 90;
-    YavasDonus.write(YavasDonusDeger);
+  if (ZamanKont < 0) {
+    YavasDonus.write(90);
   }
 
-  ZamanKont = (YatayEtkZam+ 100)- millis();
-  if(ZamanKont < 0){
-    SagKanatDeger = 90;
-    SolKanatDeger = 90;
-    SagKanat.write(SagKanatDeger);
-    SolKanat.write(SolKanatDeger);
+  ZamanKont = (YatayEtkZam + 100) - millis();
+  if (ZamanKont < 0) {
+    SagKanat.write(90);
+    SolKanat.write(90);
   }
 
-  ZamanKont = (DikeyEtkZam + 100)- millis();
-  if(ZamanKont < 0){
-    KalkisDeger = 90;
-    Kalkis.write(KalkisDeger);
+  ZamanKont = (DikeyEtkZam + 100) - millis();
+  if (ZamanKont < 0) {
+    Kalkis.write(90);
   }
-if(!radio.isChipConnected()){
-  Brushless.write(10);
-}
-    
+
+  if (!radio.isChipConnected()) {
+    Brushless.write(10);
+  }
 }
